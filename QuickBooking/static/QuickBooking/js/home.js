79ce32cc1e57cd1bc372b1924f1170149b493f2c
PaulcyMboungou <paulcy.mboungou@gmail.com>
2015-06-13 23:51:59 +0530

    Paulcy |#46, #47| Making One way and Round Trip Options in one line.

---
+++ QuickBooking/static/QuickBooking/js/home.js
@@ -0,0 +1,17 @@
+function defaultSelection() {
+	$('#oneWay').datepicker();
+    $('#roundTrip').datepicker();
+    $('#roundTrip').css("display", "none");
+}
+
+function checkTravelType(option) {
+    switch(option.value) {
+        case "One Way":
+            $('#roundTrip').css("display", "none");
+            break;
+        case "Round Trip":
+            $('#roundTrip').css("display", "block");
+            break;
+    }
+}
+
