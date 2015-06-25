ca5ea8de8a88f2fa863ed6bd61491927ce7e229d
FarelEllely <[edy.ellely@gmail.com]>
2015-06-25 17:25:57 +0530

    Jeremias, Farel |#6| , Settings buses according to date (One way).

---
+++ QuickBooking/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('QuickBooking', '0005_auto_20150625_1309'),
+        ('QuickBooking', '0003_merge'),
+    ]
+
+    operations = [
+    ]
