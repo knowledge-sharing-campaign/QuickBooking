b06e783df06d4e2e5db09b21578ad68195a6dbc7
Paulcy Mboungou <Paulcy.Mboungou@gmail.com>
2015-06-25 17:49:28 +0530

    Paulcy | Adding title on result

--- QuickBooking/views.py
+++ QuickBooking/views.py
@@ -2,8 +2,6 @@ import json
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
-from django.utils.dateformat import DateFormat
-from django.utils.formats import get_format
 
 from .models import Bus, BusStop, BusRoute
 
@@ -20,21 +18,11 @@ def index(request):
 
 def results(request):
     bus_routes = BusRoute.objects.filter(src=request.POST['src'], dest=request.POST['dest'])
-    bus_routes_date1 = request.POST['FirstDate']
-    bus_routes_date2 = request.POST['SecondDate']
-    routes = []
-
-    for route in BusRoute.objects.all():
-        date =  DateFormat(route.timing.date).format("m/d/Y")
-
-        if date == bus_routes_date1:
-            routes.append(route)
 
     template = loader.get_template('QuickBooking/results.html')
     context = RequestContext(request, {
-        'bus_routes': routes,
-        'bus_routes_date1': bus_routes_date1,
-        'bus_routes_date2': bus_routes_date2
+        'bus_routes': bus_routes
+
     })
   
     return HttpResponse(template.render(context))
