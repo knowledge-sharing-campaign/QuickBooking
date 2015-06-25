e198a28119a65a09f011087a2cb2dbd2e9ed0c3a
Paulcy Mboungou <Paulcy.Mboungou@gmail.com>
2015-06-25 13:36:37 +0530

    Paulcy | adding db sqlite file.

---
+++ QuickBooking/migrations/0003_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('QuickBooking', '0002_busroute_cost'),
+        ('QuickBooking', '0002_auto_20150623_1913'),
+    ]
+
+    operations = [
+    ]
